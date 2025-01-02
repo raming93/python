68ec47462ee2c8ba4bfb8a5b85367d3a3261141d
raming93 <raming93@gmail.com>
2025-01-02 23:26:40 +0900

    Update pythonLibSrc.py

--- pythonLibSrc.py
+++ pythonLibSrc.py
@@ -6,3 +6,14 @@ import pyautogui
 pyautogui.position()
 
 
+
+
+import xlwings as xw
+
+app = xw.App(add_book=False)
+
+wb = app.books.open('test.xlsx')
+
+wb.sheets.add('newSheet1')
+wb.sheets.add('newSheet2')
+wb.sheets.add(name='test', before= 'newSheet1')
